92b14ebb5f5b060bfc6d493ae148539980e6d3c8
pzppyzt <206838586@qq.com>
2020-12-17 19:07:17 +0800

    one

---
+++ BaseActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication.base;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract
+class BaseActivity<T extends BasePresenter> extends AppCompatActivity {
+    protected T presenter;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayout());
+        if (presenter==null){
+            presenter= getPresenter();
+        }
+        initView();
+        initData();
+    }
+
+    protected abstract void initData();
+
+    protected abstract void initView();
+
+    protected abstract T getPresenter();
+
+    protected abstract int getLayout();
+}
